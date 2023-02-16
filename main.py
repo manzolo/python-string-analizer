650f72559d58518dbd5a5707f9ca843ae2fad5b4
Andrea Manzi <manzolo@libero.it>
2023-02-16 20:38:02 +0100

    first commit

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
+import base64
 from flask import Flask, request, jsonify, render_template
 from wsgiref.simple_server import make_server
 
 app = Flask(__name__)
 port = int(os.environ.get('SERVER_PORT', 8080))
+
 
 @app.route('/api/string/analyzer')
 def analyze_string():
@@ -12,17 +14,25 @@ def analyze_string():
         return jsonify({'error': 'Missing string parameter'}), 400
 
     output_json = []
-    for char in input_string:
+
+    output_json.append({
+        'string': input_string,
+        'length': input_string.__len__(),
+        'base64': base64.b64encode(input_string.encode("ascii")).decode("ascii"),
+        'hex': "".join(hex(ord(c))[2:].zfill(2) for c in input_string)
+    })
+    for index, char in enumerate(input_string):
         byte = char.encode('utf-8')
         hex_str = byte.hex()
         bin_str = format(int(hex_str, 16), 'b')
         int_str = str(ord(char))
 
-        output_json.append({
+        output_json.append({index: {
             'char': char,
             'hex': hex_str,
             'bin': int(bin_str),
             'int': int(int_str)
+        }
         })
 
     return jsonify(output_json)
@@ -32,8 +42,9 @@ def analyze_string():
 def home():
     return render_template('index.html')
 
+
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port=8080, debug=True)
+    # app.run(host='0.0.0.0', port=8080, debug=True)
     with make_server('', port, app) as httpd:
-        print("Serving on port "+str(port)+"...")
+        print("Serving on port " + str(port) + "...")
         httpd.serve_forever()
