9106a25e87b887922dd9be4d6c5e6e27e4cecb92
Andrea Manzi <manzolo@libero.it>
2023-02-16 20:09:38 +0100

    first commit

--- test.sh
+++ test.sh
@@ -1,15 +1,3 @@
 curl http://localhost:8080/
 
-# Generare un token JWT valido
-TOKEN=$(curl -u user1:password1 http://localhost:8080/login | jq -r '.token')
-
-# Effettuare una richiesta GET all'endpoint '/protected' includendo il token JWT nell'header Authorization
-curl -H "Authorization: Bearer $TOKEN" http://localhost:8080/protected
-
-curl http://localhost:8080/protected
-
-# Generare un token JWT non valido
-TOKEN="invalid_token"
-
-# Effettuare una richiesta GET all'endpoint '/protected' includendo il token JWT non valido nell'header Authorization
-curl -H "Authorization: Bearer $TOKEN" http://localhost:8080/protected
+curl http://localhost:8080/api/string/analyzer?string=Manzolo
